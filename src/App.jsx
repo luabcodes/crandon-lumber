96087195741579ade66407ad2a9ab9287b892193
Luab <luabentes94@gmail.com>
2025-12-29 02:00:41 -0500

    Initial commit - Crandon Lumber Co website

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Products from "./components/Products";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <Hero />
+        <Products />
+        <About />
+        <Contact />
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
